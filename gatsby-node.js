798cc281560f4e00e1abdaee6a58cdebe9b6b599
Serg <s.nikolaevich123@gmail.com>
2020-05-07 15:23:12 +0300

    Catalog with pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,48 @@
  */
 
 // You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `ContentfulPage` && node.url) {
+    console.log(node, getNode, actions)
+    const value = createFilePath({ node, getNode })
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const path = require('path')
+  const result = await graphql(`
+    query {
+      allContentfulPage {
+        edges {
+          node {
+            slug
+            h1
+            title
+            seoText {
+              seoText
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allContentfulPage.edges.forEach(({ node }) => {
+    createPage({
+      path: node.slug,
+      component: path.resolve(`./src/templates/page.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        node: node,
+      },
+    })
+  })
+}
